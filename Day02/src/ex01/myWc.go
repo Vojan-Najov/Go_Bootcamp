b7c5bd87b1dc5c0621a5cb9d165c7c9d1a282f33
Vojan Najov <serejah.mashichev@gmail.com>
2024-02-23 00:23:06 +0300

    Add myWc

---
+++ Day02/src/ex01/myWc.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+  "os"
+  "fmt"
+  "sync"
+  "bufio"
+  "myWc/settings"
+)
+
+func main() {
+  settings := settings.New(os.Args[1:])
+  if settings == nil {
+    os.Exit(1)
+  }
+
+  split := bufio.ScanLines
+  if settings.CountCharacters {
+    split = bufio.ScanRunes
+  } else if settings.CountWords {
+    split = bufio.ScanWords
+  }
+
+  var wg sync.WaitGroup
+  for _, filename := range settings.Filenames {
+    file := os.Stdin
+    if filename != "-" {
+      var err error
+      file, err = os.Open(filename)
+      if err != nil {
+        fmt.Fprintln(os.Stderr, err)
+        continue
+      }
+      defer file.Close()
+    } else {
+      filename = "stdin"
+    }
+
+    fileScanner := bufio.NewScanner(file)
+    fileScanner.Split(split)
+
+    wg.Add(1)
+    go wc(&wg, filename, fileScanner)
+  }
+  wg.Wait()
+}
+
+func wc(wg *sync.WaitGroup, filename string, scanner *bufio.Scanner) {
+  var count uint
+  for scanner.Scan() {
+    count++
+  }
+  fmt.Printf("%d\t%s\n", count, filename)
+  wg.Done()
+}
+
---
+++ Day02/src/ex01/settings/settings.go
@@ -0,0 +1,74 @@
+package settings
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+)
+
+type Settings struct {
+	Filenames       []string
+	CountLines      bool
+	CountCharacters bool
+	CountWords      bool
+}
+
+func (s *Settings) handleL(str string) error {
+	if str != "true" {
+		return errors.New("incorrect value")
+	}
+	s.CountLines = true
+	return nil
+}
+
+func (s *Settings) handleW(str string) error {
+	if str != "true" {
+		return errors.New("incorrect value")
+	}
+	s.CountWords = true
+	return nil
+}
+
+func (s *Settings) handleM(str string) error {
+	if str != "true" {
+		return errors.New("incorrect value")
+	}
+	s.CountCharacters = true
+	return nil
+}
+
+func New(args []string) *Settings {
+	var stngs Settings
+
+	flagset := flag.NewFlagSet("myWc", flag.ContinueOnError)
+	flagset.SetOutput(os.Stderr)
+
+	flagset.BoolFunc("l", "Count lines", stngs.handleL)
+	flagset.BoolFunc("m", "Count characters", stngs.handleM)
+	flagset.BoolFunc("w", "Count words", stngs.handleW)
+
+	if err := flagset.Parse(args); err != nil {
+		fmt.Fprintln(os.Stderr, "%v\n", err)
+		return nil
+	}
+
+  if stngs.CountLines && (stngs.CountWords || stngs.CountCharacters) ||
+     stngs.CountWords && stngs.CountCharacters {
+    fmt.Fprintln(os.Stderr, "A single flag is expected: -l, -w or -m")
+		flagset.PrintDefaults()
+		return nil
+  }
+
+  if !(stngs.CountLines || stngs.CountWords || stngs.CountCharacters) {
+    stngs.CountWords = true  
+  }
+
+  if flagset.NArg() == 0 {
+    stngs.Filenames = []string{"-"}
+  } else {
+    stngs.Filenames = flagset.Args()
+  }
+
+	return &stngs
+}
